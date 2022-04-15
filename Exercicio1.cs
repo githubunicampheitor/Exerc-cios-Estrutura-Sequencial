b1c09d96791943ee73cfb4555554ae1d572ecfb0
Heitor Gomes <99834834+githubunicampheitor@users.noreply.github.com>
2022-04-14 23:51:30 -0300

    .

---
+++ Exercicio1.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Exercicio1 {
+  public static void Main (string[] args) {
+    int a, b, soma;
+
+    a = int.Parse(Console.ReadLine());
+    b = int.Parse(Console.ReadLine());
+    soma = a + b;
+    
+    Console.WriteLine(soma);
+  }
+}
